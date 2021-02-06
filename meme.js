dcd231fddf83f4cf75e746a1c26a19b790619867
Andrew Wladis <andrewwladis@Andrews-MacBook-Air.local>
2021-02-06 08:16:43 -0500

    added mike wazowski meme

--- meme.js
+++ meme.js
@@ -1,6 +1,6 @@
 function changeMe() {
     let meme = document.getElementById('meme');
-    let randomNum = Math.floor(Math.random() * 109);
+    let randomNum = Math.floor(Math.random() * 110);
     console.log()
     meme.src = `Memes/${randomNum}.jpeg`
 
