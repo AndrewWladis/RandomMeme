d9b59ce63222aef9256899a864810e0522b3e3c0
Andrew Wladis <andrewwladis@Andrews-MacBook-Air.local>
2021-02-12 11:06:43 -0500

    added speed update

--- meme.js
+++ meme.js
@@ -1,6 +1,6 @@
 function changeMe() {
     let meme = document.getElementById('meme');
-    let randomNum = Math.floor(Math.random() * 133);
+    let randomNum = Math.floor(Math.random() * 140);
     console.log()
     meme.src = `Memes/${randomNum}.jpeg`
 
