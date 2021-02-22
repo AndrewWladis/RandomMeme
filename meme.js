32d36e225f0fde7c59a938e11bc4f316a79faf0c
Andrew Wladis <andrewwladis@Andrews-MacBook-Air.local>
2021-02-22 09:46:19 -0500

    2/22/21 update

--- meme.js
+++ meme.js
@@ -1,6 +1,6 @@
 function changeMe() {
     let meme = document.getElementById('meme');
-    let randomNum = Math.floor(Math.random() * 200);
+    let randomNum = Math.floor(Math.random() * 210);
     console.log()
     meme.src = `Memes/${randomNum}.jpeg`
 
