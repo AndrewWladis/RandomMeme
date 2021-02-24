40089c2d208b2ce54af70b621f646c89be3e210e
Andrew Wladis <andrewwladis@Andrews-MacBook-Air.local>
2021-02-23 20:57:49 -0500

    added firefox update

--- meme.js
+++ meme.js
@@ -1,6 +1,6 @@
 function changeMe() {
     let meme = document.getElementById('meme');
-    let randomNum = Math.floor(Math.random() * 210);
+    let randomNum = Math.floor(Math.random() * 258);
     console.log()
     meme.src = `Memes/${randomNum}.jpeg`
 
