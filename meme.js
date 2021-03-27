7be5a1b480d506782cf75d9b1166419380489690
Andrew Wladis <andrewwladis@Andrews-MacBook-Air.local>
2021-03-26 20:40:38 -0400

    Mr. Worldwide Update! 🌎

--- meme.js
+++ meme.js
@@ -1,7 +1,6 @@
 function changeMe() {
     let meme = document.getElementById('meme');
-    let randomNum = Math.floor(Math.random() * 335);
+    let randomNum = Math.floor(Math.random() * 351);
     console.log()
     meme.src = `Memes/${randomNum}.jpeg`
-
 }
