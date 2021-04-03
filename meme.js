1e17e6af52041dd230239b0edbb5978e11a27f96
Andrew Wladis <andrewwladis@Andrews-MacBook-Air.local>
2021-04-03 11:28:06 -0400

    Bug fixes

--- meme.js
+++ meme.js
@@ -1,6 +1,6 @@
 function changeMe() {
     let meme = document.getElementById('meme');
-    let randomNum = Math.floor(Math.random() * 403);
+    let randomNum = Math.floor(Math.random() * 429);
     console.log()
     meme.src = `Memes/${randomNum}.jpeg`
 }
